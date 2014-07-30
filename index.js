8b83273b5027de75c6f8a19414e4a3bbd40cdf1e
Lamasu <rafaerthal@gmail.com>
2014-07-30 02:47:22 -0300

    comunication with auth server

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-var VError, request, async, uri;
-
+var VError, httpRequest, async, uri;
 VError = require('verror');
-request = require('request');
+httpRequest = require('request');
 async = require('async');
 
-/**
- * @method
- * @summary Setups default base uri
- *
- * @param {String} newUri
- */
 exports.configUri = function (newUri) {
   'use strict';
-  
+
   uri = newUri;
+};
+
+exports.session = function authSession() {
+  'use strict';
+
+  return function authSessionMiddleware(request, response, next) {
+    var token;
+    token = request.get('csrf-token');
+    httpRequest({
+      'url'     : uri + '/users/me',
+      'headers' : {'csrf-token' : token}
+    }, function (error, res, body) {
+      var user;
+      if (error) {
+        return next(error);
+      }
+      try {
+        user = JSON.parse(body);
+        request.session = user;
+        return next();
+      } catch (error) {
+        return next(error);
+      }
+    });
+  };
+};
+
+exports.can = function authCan(permission) {
+  'use strict';
+
+  return function authPermissionMiddleware(request, response, next) {
+    var session;
+    session = request.session;
+    if (!session || session.profile.permissions.lastIndexOf(permission) === -1) {
+      return response.status(403).end();
+    }
+    return next();
+  };
 };
